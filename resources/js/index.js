7d002d99a4b90ec662c8ac03c1a96253243d5b3f
ksj0818 <ksj071808@gmail.com>
2021-09-15 14:40:18 +0900

    투두리스트복습

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,20 +1,32 @@
-const addTodoBtn = document.getElementById('addTodoBtn');
-const todoInput = document.getElementById('todoInput');
-const todoItem = document.getElementById('todoItem');
+const saveTodo = document.getElementById('saveTodo');
+const inputTodo = document.getElementById('inputTodo');
+const itemTodo = document.getElementById('itemTodo');
 
 let todoList = [];
 
 
-addTodoBtn.addEventListener('click', function() {
-  let todoText = todoInput.value;
-  
+saveTodo.addEventListener('click', function() {
+  let todoText = inputTodo.value;
+
   todoList.push(todoText)
-  todoInput.value = '';
+  inputTodo.value = '';
 
   fetchList();
 });
 
+const templete = `
+<div class="card mb-3" >
+  <div class="card-body">
+    <div class="row">
+      <div class="col-1 cursor-pointer first__opacity"><i class="fas fa-check" id="checkIcon"></i></div>
+      <div class="col-10 cursor-pointer" id="todoText">text</div>
+      <div class="col-1 cursor-pointer first__opacity"><i class="fas fa-trash" id="trashIcon"></i></div>
+    </div>
+  </div>
+</div>
+`
+
 // functions
 function fetchList() {
-  
+
 }
