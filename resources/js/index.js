6e8baaefd78d4c3cc892024569fddf97b59bcc9d
ksj0818 <ksj071808@gmail.com>
2021-09-15 13:41:32 +0900

    Create todoList Review

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,87 +1,20 @@
-let todoList = []
-const inputTodo = document.getElementById('inputTodo')
-const saveTodo = document.getElementById('saveTodo')
-const listTodo = document.getElementById('listTodo')
+const addTodoBtn = document.getElementById('addTodoBtn');
+const todoInput = document.getElementById('todoInput');
+const todoItem = document.getElementById('todoItem');
 
-if (localStorage.todo != undefined) {
-  todoList = JSON.parse(localStorage.todo)
-  fetchList()
-}
+let todoList = [];
 
-saveTodo.addEventListener("click", function() {  
-  let todoText = inputTodo.value
-  let todoItem = {
-    text: todoText,
-    complete: false
-  }
-  todoList.push(todoItem)
-  setLocalStorage()
 
-  inputTodo.value = ''
-  fetchList()
-})
+addTodoBtn.addEventListener('click', function() {
+  let todoText = todoInput.value;
+  
+  todoList.push(todoText)
+  todoInput.value = '';
 
-const templateListTodo = `
-<div class="card">
-  <div class="card-body">
-    <div class="row">
-      <div class="col-10">프로그래밍</div>
-      <div class="col-2">
-        <button>X</button>
-      </div>
-    </div>
-  </div>
-</div>
-`
+  fetchList();
+});
 
+// functions
 function fetchList() {
-  let html = ''
-  for (let index in todoList) {
-    let todoElement = '' 
-    todoElement = todoList[index].complete ? 
-    `<div class="selectTodo col-10 text-decoration-line-through" data-index=${index}>${todoList[index].text}</div>` :
-    `<div class="selectTodo col-10" data-index=${index}>${todoList[index].text}</div>`
-    
-    const templateListTodo = `
-      <div class="card mb-2">
-        <div class="card-body">
-          <div class="row">
-            ${todoElement}
-            <div class="col-2">
-              <button class="deleteButton" data-index=${index}>X</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    `
-    html += templateListTodo
-  }
-  listTodo.innerHTML = html
-
-  let deleteButtons = document.getElementsByClassName('deleteButton')
-  for (let deleteButton of deleteButtons) {
-    deleteButton.addEventListener('click', function() {
-      const index = Number(this.dataset.index)
-      todoList.splice(index, 1)
-      setLocalStorage()
-      fetchList()
-    })
-  }
-
-  let selectTodos = document.getElementsByClassName('selectTodo')
-  for (let selectTodo of selectTodos) {
-    selectTodo.addEventListener('click', function() {
-      const index = Number(this.dataset.index)
-      todoList[index].complete = !todoList[index].complete
-      setLocalStorage()
-      fetchList()
-    })
-  }
-}
-
-function setLocalStorage() {
-  if (localStorage.todo != undefined) {
-    localStorage.removeItem('todo')
-  }
-  localStorage.setItem('todo', JSON.stringify(todoList))
+  
 }
