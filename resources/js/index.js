79b6911db8f1c4002c73d63031939bf526f3bc38
ksj0818 <ksj071808@gmail.com>
2021-09-15 11:21:28 +0900

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,87 +1,58 @@
-let todoList = []
-const inputTodo = document.getElementById('inputTodo')
-const saveTodo = document.getElementById('saveTodo')
-const listTodo = document.getElementById('listTodo')
+let todoList = [];
+let todoInput = document.getElementById('todoInput');
+const todoSave = document.getElementById('todoSave');
+const todoItem = document.getElementById('todoItem');
 
-if (localStorage.todo != undefined) {
-  todoList = JSON.parse(localStorage.todo)
-  fetchList()
-}
 
-saveTodo.addEventListener("click", function() {  
-  let todoText = inputTodo.value
-  let todoItem = {
-    text: todoText,
+todoSave.addEventListener('click', function() {
+  const todo = {
+    text: todoInput.value,
     complete: false
   }
-  todoList.push(todoItem)
-  setLocalStorage()
+  todoList.push(todo);
+  todoInput.value = '';
+  fetchList();
+});
 
-  inputTodo.value = ''
-  fetchList()
-})
-
-const templateListTodo = `
-<div class="card">
-  <div class="card-body">
-    <div class="row">
-      <div class="col-10">프로그래밍</div>
-      <div class="col-2">
-        <button>X</button>
-      </div>
-    </div>
-  </div>
-</div>
-`
-
+// functions
 function fetchList() {
   let html = ''
   for (let index in todoList) {
-    let todoElement = '' 
-    todoElement = todoList[index].complete ? 
-    `<div class="selectTodo col-10 text-decoration-line-through" data-index=${index}>${todoList[index].text}</div>` :
-    `<div class="selectTodo col-10" data-index=${index}>${todoList[index].text}</div>`
-    
-    const templateListTodo = `
-      <div class="card mb-2">
-        <div class="card-body">
-          <div class="row">
-            ${todoElement}
-            <div class="col-2">
-              <button class="deleteButton" data-index=${index}>X</button>
-            </div>
-          </div>
-        </div>
+    let todoComplete = todoList[index].complete ? 
+    `<div class="col-10 selectItem text-decoration-line-through" data-index="${index}">
+      ${todoList[index].text}
+    </div>` :
+    `<div class="col-10 selectItem" data-index="${index}">
+      ${todoList[index].text}
+    </div>`
+
+    let todoTemplete = `
+      <div class="row mb-2">
+        ${todoComplete}
+      <div class="col-2">
+        <button class="deleteItem" data-index="${index}">x</button>
+      </div>
       </div>
     `
-    html += templateListTodo
+    html += todoTemplete;
   }
-  listTodo.innerHTML = html
+  todoItem.innerHTML = html;
 
-  let deleteButtons = document.getElementsByClassName('deleteButton')
-  for (let deleteButton of deleteButtons) {
-    deleteButton.addEventListener('click', function() {
-      const index = Number(this.dataset.index)
-      todoList.splice(index, 1)
-      setLocalStorage()
-      fetchList()
-    })
+  const deleteItems = document.getElementsByClassName('deleteItem');
+  for (let deleteItem of deleteItems) {
+    deleteItem.addEventListener('click', function() {
+      todoList.splice(this.dataset.index, 1);
+      fetchList();
+    });
   }
-
-  let selectTodos = document.getElementsByClassName('selectTodo')
-  for (let selectTodo of selectTodos) {
-    selectTodo.addEventListener('click', function() {
-      const index = Number(this.dataset.index)
-      todoList[index].complete = !todoList[index].complete
-      setLocalStorage()
-      fetchList()
-    })
+  
+  const selectItems = document.getElementsByClassName('selectItem');
+  for (let selectItem of selectItems) {
+    selectItem.addEventListener('click', function() {
+      todoList[this.dataset.index].complete = !todoList[this.dataset.index].complete
+      fetchList();
+    });
   }
 }
 
-function setLocalStorage() {
-  if (localStorage.todo != undefined) {
-    localStorage.removeItem('todo')
-  }
-  localStorage.setItem('todo', JSON.stringify(todoList))
-}
+
