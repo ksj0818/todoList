f88788debf18a67b63e0cbfc48994240a2db1945
ksj0818 <ksj071808@gmail.com>
2021-09-16 19:04:18 +0900

    투두리스트 복습3

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,77 +1,62 @@
-const todoSaveBtn = document.getElementById('todo-save-btn');
+const todoSave = document.getElementById('todo-save');
 const todoInput = document.getElementById('todo-input');
-const todoListView = document.getElementById('todo-list-view');
+const todoListItem = document.getElementById('todo-list__item');
+
 let todoList = [];
 
-if (localStorage.todo != undefined) {
-  todoList = JSON.parse(localStorage.todo);
-  fetchList();
-}
-
-todoSaveBtn.addEventListener('click', function() {
+todoSave.addEventListener('click', function() {
   let todoItems = {
     text: todoInput.value,
     complete: false
   }
 
   todoList.push(todoItems)
-  setLocalStorage();
-
+  
   todoInput.value = '';
-
   fetchList();
 });
 
-// functions
-function fetchList() {
-  let html = '';
-  for (let index in todoList) {
-    let todoElement = '';
-    todoElement = todoList[index].complete ?
-    `<div class="col-11 select-todo text-decoration-line-through" data-index=${index}>${todoList[index].text}</div>` :
-    `<div class="col-11 select-todo" data-index=${index}>${todoList[index].text}</div>`
 
-    const todoListTemplate = `
-      <div class="card mb-2">
+
+// functions 
+function fetchList() {
+  let htmlBox = '';
+  for (let index in todoList) { 
+    const todoTemplate = `
+      <div class="card mb-3">
         <div class="card-body">
           <div class="row">
-            ${todoElement}
-            <div class="col-1 deleteTodoBtn" data-index=${index}>
-              <i class="fas fa-trash"></i>
+            <div class="col-1">
+              <i class="fas fa-check check-todo" onclick="checkTodo(${index})"></i>
+            </div>
+            <div class="col-10 todo-text">${todoList[index].text}</div>
+            <div class="col-1">
+              <i class="fas fa-trash delete-todo__btn" onclick="deleteTodo(${index})"></i>
             </div>
           </div>
         </div>
       </div>
     `;
-    html += todoListTemplate;
+    htmlBox += todoTemplate;
   }
-  todoListView.innerHTML = html;
-
-  let deleteTodoBtns = document.getElementsByClassName('deleteTodoBtn');
-  for (let deleteTodoBtn of deleteTodoBtns) {
-    deleteTodoBtn.addEventListener('click', function() {
-      const index = Number(this.dataset.index);
-      todoList.splice(index, 1);
-      setLocalStorage()
-      fetchList();
-    });
-  }
-
-  let selectTodos = document.getElementsByClassName('select-todo');
-  for (selectTodo of selectTodos) {
-    selectTodo.addEventListener('click', function() {
-      const index = Number(this.dataset.index);
-      todoList[index].complete = !todoList[index].complete;
-      setLocalStorage()
-      fetchList()
-    });
-  }
-} // end fetchList()
-
-function setLocalStorage() {
-  if (localStorage.todo != undefined) {
-    localStorage.removeItem('todo');
-  }
-  localStorage.setItem('todo', JSON.stringify(todoList));
+  todoListItem.innerHTML = htmlBox;
 }
 
+function checkTodo(index) {
+  let todoText = document.getElementsByClassName('todo-text');
+  let checkTodo = document.getElementsByClassName('check-todo');
+
+  todoList[index].complete = !todoList[index].complete;
+  if (todoList[index].complete === true) {
+    todoText[index].className = "col-10 todo-text text-decoration-line-through";
+    checkTodo[index].className = 'fas fa-check check-todo complete-check';
+  } else {
+    todoText[index].className = "col-10 todo-text";
+    checkTodo[index].className = 'fas fa-check check-todo';
+  }
+}
+
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  fetchList();
+} 
