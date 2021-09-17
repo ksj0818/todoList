92e0dcf9b11197282263850ae258f0c5c5de7764
ksj0818 <ksj071808@gmail.com>
2021-09-17 14:36:57 +0900

    todoText 업데이트 기능 추가

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,14 @@
 const todoSave = document.getElementById('todo-save');
 const todoInput = document.getElementById('todo-input');
 const todoListItem = document.getElementById('todo-list__item');
-
+const todoText = document.getElementsByClassName('todo-text');
+const checkTodo = document.getElementsByClassName('check-todo');
 let todoList = [];
+
+if (localStorage.todo !== undefined) {
+  todoList = JSON.parse(localStorage.todo);
+  fetchList();
+}
 
 todoSave.addEventListener('click', function() {
   let todoItems = {
@@ -13,12 +19,12 @@ todoSave.addEventListener('click', function() {
   todoItems.text === '' || todoItems.text === null ?
   alert('할 일을 입력해 주세요.') :
   todoList.push(todoItems);
-  
+
   todoInput.value = '';
+  setLocalStorage();
+
   fetchList();
 });
-
-
 
 // functions 
 function fetchList() {
@@ -29,9 +35,9 @@ function fetchList() {
         <div class="card-body">
           <div class="row">
             <div class="col-1">
-              <i class="fas fa-check check-todo" onclick="checkTodo(${index})"></i>
+              <i class="fas fa-check ${todoList[index].complete === true ? 'check-todo complete-check' : 'check-todo'}" onclick="checkTodo1(${index})"></i>
             </div>
-            <div class="col-10 todo-text">${todoList[index].text}</div>
+            <div class="col-10 ${todoList[index].complete === true ? 'todo-text text-decoration-line-through' : 'todo-text'}" onclick="updateTodoText(${index})">${todoList[index].text}</div>
             <div class="col-1">
               <i class="fas fa-trash delete-todo__btn" onclick="deleteTodo(${index})"></i>
             </div>
@@ -44,10 +50,7 @@ function fetchList() {
   todoListItem.innerHTML = htmlBox;
 }
 
-function checkTodo(index) {
-  let todoText = document.getElementsByClassName('todo-text');
-  let checkTodo = document.getElementsByClassName('check-todo');
-
+function checkTodo1(index) {
   todoList[index].complete = !todoList[index].complete;
   if (todoList[index].complete === true) {
     todoText[index].className = "col-10 todo-text text-decoration-line-through";
@@ -56,10 +59,28 @@ function checkTodo(index) {
     todoText[index].className = "col-10 todo-text";
     checkTodo[index].className = 'fas fa-check check-todo';
   }
+  setLocalStorage();
 }
 
 function deleteTodo(index) {
-  todoList.splice(index, 1);
+  confirm('정말 삭제할까요?') === true ? 
+  todoList.splice(index, 1)
+  : null;
+  setLocalStorage();
   fetchList();
 } 
 
+function setLocalStorage() {
+  if (localStorage.todo !== undefined) {
+    localStorage.removeItem('todo');
+  }
+  localStorage.setItem('todo', JSON.stringify(todoList));
+}
+
+function updateTodoText(index) {
+  let updateText = prompt('수정할 내용을 입력해 주세요.', todoList[index].text );
+  todoList[index].text = updateText !== null ? updateText : todoList[index].text;
+  console.log(todoList[index].text)
+  setLocalStorage();
+  fetchList();
+}
